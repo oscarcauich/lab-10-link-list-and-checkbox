440ca300ac913eb0b5e1b02aebdce81647c2d590
Oscar Cauich <ocauich@gravitypayments.com>
2017-06-26 13:58:12 -0700

    Lab not Done

--- lab-oscar/index.js
+++ lab-oscar/index.js
@@ -15,23 +15,30 @@ class Node {
     this.next.appendNode(value);
   }
   removeNode(value){
-    let tempNode = this;
-    let prevNode = null;
+  let currentNode = this
+  let previousNode = null;
+  let notFoundYet = null;
 
-    if(tempNode.value == value){
-      head = this.next;
-      tempNode = null ;
+    if(currentNode.value == value){
+      currentNode.value = null;
+      currentNode = currentNode.next;
       return;
     }
-    while(tempNode.next !== null && tempNode.value != value){
-      prevNode = tempNode;
-      tempNode = tempNode.next;
-    }
-    if(tempNode.next === null){
-      return console.log('not found');
-    }
-    prevNode.next = tempNode.next;
 
+    while(!notFoundYet){
+      if(currentNode.value == value){
+        currentNode.value = null;
+        previousNode.next = currentNode.next;
+        notFoundYet = true;
+        return;
+      }
+
+      previousNode = currentNode;
+      currentNode = currentNode.next
+
+    }
+
+    return;
   }
 }
 
@@ -43,18 +50,21 @@ try {
   console.log('runnint test, please wait...');
 
   let head = new Node(1);
-  console.log(head);
+  // console.log(head);
   console.assert(head.value == 1);
   console.assert(head.next == null);
 
   head.appendNode(new Node(2));
   head.appendNode(new Node(3));
-  console.log(head);
+  // console.log(head);
   console.assert(head.value == 1);
   console.assert(head.next.value == 2);
   console.assert(head.next.next.value == 3);
   console.dir(head, {depth: 100});
 
+  head.removeNode(head.value);
+
+  console.log('new head', head);
 }catch(err){
   console.error('It failed', err);
 }
